44fc3a7f88f2ca2147a2f75f8905f47fc89b0b4f
oswaldoacauan <oswaldoacauan@gmail.com>
2013-09-25 21:05:26 -0300

    Update plugin filenames and add reading-time and url helper plugins

---
+++ app/_plugins/filter_url_helper.rb
@@ -0,0 +1,48 @@
+# Jekyll URL Helper Filter Plugin with CDN Support
+# Version: 1.0.0
+# Release Date: 2013-05-24
+# License: MIT
+# (c) Copyright Jhaura Wachsman, http://jhaurawachsman.com
+# Adapted for JPress, http://getjpress.com
+module Jekyll
+  module Filters
+
+    # See the README for usage examples.
+
+    # The get_ methods grabs/returns a value from _config.yml.
+
+    # Internal: Get the 'url' variable value from _config.yml.
+    def get_url
+      url = @context.registers[:site].config['url']
+    end
+
+    # Internal: Get the 'baseurl' variable value from _config.yml.
+    def get_baseurl
+      baseurl = @context.registers[:site].config['baseurl']
+    end
+
+    # The to_ methods transform a root-relative path into the path
+    # type of the method name: to_<type>url.
+
+    # Public: Append the 'baseurl' variable to 'input'.
+    def to_baseurl(input)
+
+      # baseurl << input # wouldn't work, created a huge concat chain
+      input = "#{get_baseurl}#{input}"
+    end
+
+    # Public: Append the 'url' variable to 'input'.
+    def to_absurl(input)
+
+      # url + baseurl << input
+      input = "#{get_url}#{get_baseurl}#{input}"
+    end
+
+    # Public: Sanitize a string for use in a URL.
+    def sanitize_str(input)
+      input.gsub(/[^a-z0-9 -]+/, '').gsub(/\s/, '-').gsub(/-{2,}/, '-').downcase
+    end
+  end
+end
+
+Liquid::Template.register_filter(Jekyll::Filters)
